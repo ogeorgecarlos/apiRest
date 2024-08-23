a0520e49ac05edacafb21a402a93b9525b552309
george gomes <georgecarlos@live.com>
2024-08-23 20:08:12 +0100

    criada primeira migration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 const port = 3001;
 
-app.listen(3001, ()=>{
+app.listen(port, ()=>{
   console.log(`Escultando na porta ${port}`);
   console.log(`CTRL + Clique em http://localhost: ${port}`);
 });
