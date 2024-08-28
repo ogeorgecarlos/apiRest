e61863e085d3f5fad8f1db1853525689ee11aed5
george gomes <georgecarlos@live.com>
2024-08-28 13:01:43 +0100

    included check token to all user routes, except to create a new user.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,9 +7,9 @@ import TokenController from "../controllers/TokenController";
 const router = new express.Router();
 
 router.get("/get", requireEmailPassword, TokenController.checkToken, checkSameUser, userController.show);
-router.get("/get-all", requireEmailPassword, checkSameUser,userController.index);
-router.put("/update", requireEmailPassword, checkSameUser, userController.update);
+router.get("/get-all", requireEmailPassword, TokenController.checkToken, checkSameUser,userController.index);
+router.put("/update", requireEmailPassword, TokenController.checkToken, checkSameUser, userController.update);
 router.post("/create", userController.store);
-router.delete("/delete", requireEmailPassword, checkSameUser, userController.delete);
+router.delete("/delete", requireEmailPassword, TokenController.checkToken, checkSameUser, userController.delete);
 
 export default router;
