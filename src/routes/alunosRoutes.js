cdbb46cc26cf4b1f1d25a2b96d53119fb473fad4
george gomes <georgecarlos@live.com>
2024-08-30 10:22:48 +0100

    included requiredCredentials middleware on all the alunoRoutes

--- src/routes/alunosRoutes.js
+++ src/routes/alunosRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 import AlunosController from "../controllers/AlunoController";
+import requireEmailPassword from "../midllewares/requireEmailPassword";
 
 const router = new express.Router();
 
-router.get("/get", AlunosController.show);
-router.get("/get-all", AlunosController.index);
-router.post("/create", AlunosController.store);
-router.put("/update", AlunosController.update);
-router.delete("/delete", AlunosController.delete);
+router.get("/get", requireEmailPassword, AlunosController.show);
+router.get("/get-all", requireEmailPassword, AlunosController.index);
+router.post("/create", requireEmailPassword, AlunosController.store);
+router.put("/update", requireEmailPassword, AlunosController.update);
+router.delete("/delete", requireEmailPassword, AlunosController.delete);
 
 export default router;
