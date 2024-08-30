0c933d19a56b1f913f69a933bd3b088eb273227a
george gomes <georgecarlos@live.com>
2024-08-30 10:28:15 +0100

    included token required to alunoRoutes

--- src/routes/alunosRoutes.js
+++ src/routes/alunosRoutes.js
@@ -2,13 +2,14 @@ import express from "express";
 
 import AlunosController from "../controllers/AlunoController";
 import requireEmailPassword from "../midllewares/requireEmailPassword";
+import TokenController from "../controllers/TokenController";
 
 const router = new express.Router();
 
-router.get("/get", requireEmailPassword, AlunosController.show);
-router.get("/get-all", requireEmailPassword, AlunosController.index);
-router.post("/create", requireEmailPassword, AlunosController.store);
-router.put("/update", requireEmailPassword, AlunosController.update);
-router.delete("/delete", requireEmailPassword, AlunosController.delete);
+router.get("/get", requireEmailPassword, TokenController.checkToken, AlunosController.show);
+router.get("/get-all", requireEmailPassword, TokenController.checkToken, AlunosController.index);
+router.post("/create", requireEmailPassword, TokenController.checkToken, AlunosController.store);
+router.put("/update", requireEmailPassword, TokenController.checkToken, AlunosController.update);
+router.delete("/delete", requireEmailPassword, TokenController.checkToken, AlunosController.delete);
 
 export default router;
